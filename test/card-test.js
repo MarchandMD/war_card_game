decebc3cad626d02c490558d1e87fe7b2b24e80b
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-03-25 09:07:19 -0600

    tests card and deck

--- test/card-test.js
+++ test/card-test.js
@@ -1,21 +1,21 @@
-var assert = require("chai").assert
+var expect = require("chai").expect
 var Card = require("../lib/card.js")
 
 describe("Card", function () {
     it("should be a function", function () {
-        assert.isFunction(Card)
+      expect(Card).to.be.a('function')
     })
 
     it("should have a suit", function () {
         var card = new Card('hearts')
-        assert.equal(card.suit, 'hearts')
+        expect(card.suit).to.eq('hearts')
     })
     it("should have a value", function () {
         var card = new Card('hearts', 3)
-        assert.equal(card.value, 3)
+        expect(card.value).to.eq(3)
     })
     it("should have a rank", function () {
         var card = new Card('hearts', 3, 2)
-        assert.equal(card.rank, 2)
+        expect(card.rank).to.eq(2)
     })
 })
--- test/deck-test.js
+++ test/deck-test.js
@@ -1,20 +1,40 @@
-var assert = require("chai").assert
+var expect = require("chai").expect
 var Deck = require("../lib/deck.js")
 var Card = require("../lib/card.js")
 
 describe("Deck", function () {
-    it("should be a function", function () {
-        assert.isFunction(Deck)
-    })
 
-    it("should have an array of cards", function () {
+    let cards = null;
+    let deck = null;
+
+    beforeEach(function () {
         var card1 = new Card("hearts", "2", 1)
         var card2 = new Card("clubs", "3", 2)
         var card3 = new Card("diamonds", "Jack", 11)
-        var cards = [card1, card2, card3]
+        cards = [card1, card2, card3];
+        deck = new Deck(cards);
+    })
 
+    it("should be a function", function () {
+        expect(Deck).to.be.a("function")
+    })
+
+    it("should have an array of cards", function () {
+        expect(deck.cards).to.eq(cards)
+        expect(deck.cards.length).to.eq(3)
+    })
+
+    it("should be able to retrieve #rankOfCardAt", function () {
+        var card1 = new Card("hearts", "2", 1)
+        var cards = [card1]
         var deck = new Deck(cards)
-        assert.equal(deck.cards, cards)
-        assert.equal(deck.cards.length, 3)
+
+        expect(deck.rankOfCardAt(0)).to.eq(1)
+    })
+
+    describe("#highRankingCards", function () {
+        it("retrieves cards valued 11 or higher", function () {
+            var expected = []
+        })
     })
 })
